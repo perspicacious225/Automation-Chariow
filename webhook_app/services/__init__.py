8216e25112d271c6c0e4702e0df14a208dffb846
Perspicacious225 <hyanic.dongo@epitech.eu>
2025-08-12 16:27:54 +0000

    sheet suspend

--- webhook_app/services/__init__.py
+++ webhook_app/services/__init__.py
@@ -1,4 +1,4 @@
 from .notifier import Notifier
 from .whatsapp import WhatsAppService
-from .google_sheets import GoogleSheetsService
+# from .google_sheets import GoogleSheetsService
 from .email import EmailService
